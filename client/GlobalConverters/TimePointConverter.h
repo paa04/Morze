b57ba7407a1cb0d15e8042f1daadf6f737ae3ad9
Ivan <bugakov0601@yandex.ru>
2026-04-18 02:05:50 +0300

    converters upd

---
+++ client/GlobalConverters/TimePointConverter.h
@@ -0,0 +1,13 @@
+//
+// Created by ivan on 18.04.2026.
+//
+
+#ifndef MORZE_TIMEPOINTCONVERTER_H
+#define MORZE_TIMEPOINTCONVERTER_H
+
+
+class TimePointConverter {
+};
+
+
+#endif //MORZE_TIMEPOINTCONVERTER_H
---
+++ client/GlobalConverters/UUIDConverter.h
@@ -0,0 +1,13 @@
+//
+// Created by ivan on 18.04.2026.
+//
+
+#ifndef MORZE_UUIDCONVERTER_H
+#define MORZE_UUIDCONVERTER_H
+
+
+class UUIDConverter {
+};
+
+
+#endif //MORZE_UUIDCONVERTER_H
