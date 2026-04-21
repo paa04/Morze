21a456ae5c64777e0959c263af15b736a299ac0a
Ivan <bugakov0601@yandex.ru>
2026-04-21 12:50:45 +0300

    upd and qt

---
+++ client/services/services/SignalingService.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ivan on 21.04.2026.
+//
+
+#include "SignalingService.h"
---
+++ client/services/services/SignalingService.h
@@ -0,0 +1,13 @@
+//
+// Created by ivan on 21.04.2026.
+//
+
+#ifndef MORZE_SIGNALINGSERVICE_H
+#define MORZE_SIGNALINGSERVICE_H
+
+
+class SignalingService {
+};
+
+
+#endif //MORZE_SIGNALINGSERVICE_H
